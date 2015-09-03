05002e9c6b50aabb85a0c65e1033ed9e66422bb2
Toshiharu Takematsu <info@newfish.jp>
2015-09-03 18:52:30 +0900

    Implement: State API

--- client.go
+++ client.go
@@ -7,13 +7,13 @@ import (
 )
 
 type Client struct {
-	url      string
-	response *http.Response
+	Url      string
+	Response *http.Response
 }
 
 func OpenClient(url string) (client *Client, error error) {
 	client = new(Client)
-	client.url = url
+	client.Url = url
 	return
 }
 
@@ -38,15 +38,40 @@ type Info struct {
 }
 
 func (client *Client) Info() (info *Info, error error) {
-	response, error := http.Get(client.url + "/osc/info")
+	client.Response, error = http.Get(client.Url + "/osc/info")
 	if error != nil {
 		return
 	}
-	body, error := ioutil.ReadAll(response.Body)
+	body, error := ioutil.ReadAll(client.Response.Body)
 	if error != nil {
 		return
 	}
 	info = new(Info)
 	json.Unmarshal(body, &info)
+	return
+}
+
+type CameraState struct {
+	SessionId      string
+	BatteryLevel   float32
+	StorageChanged bool
+}
+
+type State struct {
+	Fingerprint string
+	State       CameraState
+}
+
+func (client *Client) State() (state *State, error error) {
+	client.Response, error = http.PostForm(client.Url+"/osc/state", nil)
+	if error != nil {
+		return
+	}
+	body, error := ioutil.ReadAll(client.Response.Body)
+	if error != nil {
+		return
+	}
+	state = new(State)
+	json.Unmarshal(body, &state)
 	return
 }
